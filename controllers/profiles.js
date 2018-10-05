1f85ef6e27ed78c2b3c7afa15aebf6cd847d03fa
Lincoln Choy <lincoln.choy1997@gmail.com>
2018-10-06 01:35:00 +1300

    Implemented getProfile method

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,10 +1,26 @@
 const handleGetProfile = (req, res, db, bcrypt) => {
 
-	/* Identify user from query string */
-	const user = req.query.user;
-	
+	const isInteger = Number.isInteger(parseInt(req.query.user));
 
-	return res.status(200).json('API_SUCCESS');
+	if (!isInteger) {
+		return res.status(400).json('INVALID_PARAMS');
+	}
+	/* Identify user from query string */
+	const id = req.query.user;
+	
+	db.select('*')
+	.from('profilect')
+	.where('id','=',id)
+	.then(user => {
+		return res.status(200).json({ 
+			id : user[0].id, 
+			occupation : user[0].occupation, 
+			picture : user[0].picture, 
+			blurb : user[0].blurb, 
+			birthday : user[0].birthday, 
+			location : user[0].location
+		})
+	})
 }
 
 module.exports = {
