991f9ca10ce3513d7d9f7bce3b14c0afc1015ee8
Lincoln Choy <lincoln.choy1997@gmail.com>
2018-10-08 02:31:14 +1300

    Deploy

--- server.js
+++ server.js
@@ -15,11 +15,10 @@ const messaging = require('./controllers/messaging');
 const db = knex({
 	client : 'pg',
 	connection : {
-		host : '127.0.0.1',
-		user : 'postgres',
-		password : '',
-		database : 'chat_time',
-	}	
+		connectionString : process.env.DATABASE_URL,
+		ssl : true,
+
+	}
 });
 
 const app = express();
