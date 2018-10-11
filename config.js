c861b9f7ad3152363cb177d33463dda83f2066c6
Lincoln Choy <lincoln.choy1997@gmail.com>
2018-10-12 06:14:28 +1300

    Deploy

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ const cloudinary = require('cloudinary');
 
 const config = () => {
 	cloudinary.config({ 
-		cloud_name: 'dv7e36bfu', 
-		api_key: '313464432566752', 
-		api_secret: 'A4n4q7z9wkDjhNNEVkAfJE6nDmY'
-	}
-)}
+		cloud_name: process.env.CLOUD_NAME, 
+		api_key: process.env.API_KEY, 
+		api_secret: process.env.API_SECRET
+	})
+}
 
 /* 
 cloudinary.config({ 
